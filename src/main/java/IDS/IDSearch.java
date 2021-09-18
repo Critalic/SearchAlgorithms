fe95f42eeeb5a073f57834653a88f0cee97e0102
V1tali1ty <vitalikkryvonosiuk@gmail.com>
2021-09-18 08:36:45 +0300

    Finished the IDS

--- src/main/java/IDS/IDSearch.java
+++ src/main/java/IDS/IDSearch.java
@@ -1,13 +1,11 @@
 package IDS;
 
 public class IDSearch {
-    final int N = 8;
-
     /* A utility function to print solution */
     void printSolution(int board[][])
     {
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++)
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++)
                 System.out.print(" " + board[i][j]
                         + " ");
             System.out.println();
@@ -27,6 +25,10 @@ public class IDSearch {
         for (i = col+1; i < board.length; i++)
             if (board[row][i] == 1)
                 return false;
+        /* Check this row on left side */
+        for (i = col-1; i >=0; i--)
+            if (board[row][i] == 1)
+                return false;
 
         /* Check upper diagonal on right side */
         for (i = row-1, j = col+1; i >=0 && j < board.length; i--, j++)
@@ -38,6 +40,36 @@ public class IDSearch {
             if (board[i][j] == 1)
                 return false;
 
+        /* Check upper diagonal on left side */
+        for (i = row-1, j = col-1; i >=0 && j >=0; i--, j--)
+            if (board[i][j] == 1)
+                return false;
+
+        /* Check lower diagonal on left side */
+        for (i = row+1, j = col-1; j >=0 && i < board.length; i++, j--)
+            if (board[i][j] == 1)
+                return false;
+
+        return true;
+    }
+    int findSafe(int[][]board, int col) {
+        for(int i =col+1; i< board.length; i++) {
+            for(int b =0; b<board.length; b++) {
+                if(isSafe(board, b, i) && board[b][i]!=1) return i;
+            }
+        }
+        return 7;
+    }
+
+    boolean checkIfInFinalState (int[][] board) {
+        for(int col =0; col<board.length; col++) {
+            for(int row =0; row<board[col].length; row++) {
+                if(board[row][col] ==1) {
+                    if(isSafe(board, row, col)) continue;
+                    return false;
+                }
+            }
+        }
         return true;
     }
 
@@ -52,19 +84,18 @@ public class IDSearch {
     }
 
     State depthLimitedSearch (int[][] board, int limit, int column, int iteration) {
-//        printSolution(board);
-//        System.out.println();
+        printSolution(board);
+        System.out.println();
+        if(checkIfInFinalState(board)) return State.TRUE;
         // if the board's end has been reached then all queens are placed properly
-        if(column >= board.length) return State.TRUE;
+        if(column >= board.length) column=0;
         // if the limit has been exceeded return cutoff
         if(iteration>=limit) return State.CUTOFF;
         // find the queen in the current column and recur forward if safe OR replace queen with 0 and go on
         for(int i=0; i<board.length; i++) {
             if(board[i][column]==1) {
                 if(isSafe(board, i, column)) {
-                    State s = depthLimitedSearch(board, limit, column+1, iteration+1);
-                    if(s.equals(State.TRUE)) return State.TRUE;
-                    if(s.equals(State.CUTOFF)) return State.CUTOFF;
+                    return depthLimitedSearch(board, limit, column+1, iteration+1);
                 }
                 board[i][column] =0;
                 break;
@@ -74,25 +105,26 @@ public class IDSearch {
         for(int i=0; i<board.length; i++) {
             if(isSafe(board, i, column)) {
                 board[i][column] = 1;
-                State state = depthLimitedSearch(board, limit ,column+1, iteration+1);
-                if(state.equals(State.TRUE)) return State.TRUE;
-                if(state.equals(State.CUTOFF)) return State.CUTOFF;
+                return depthLimitedSearch(board, limit ,column+1, iteration+1);
+
             }
         }
         // find safe place for the queen (from bottom to top) and recur to the next column
         for(int i=board.length-1; i>=0; i--) {
             if(isSafe(board, i, column)) {
                 board[i][column] = 1;
-                State state = depthLimitedSearch(board, limit ,column+1, iteration+1);
-                if(state.equals(State.TRUE)) return State.TRUE;
-                if(state.equals(State.CUTOFF)) return State.CUTOFF;
+                return depthLimitedSearch(board, limit ,column+1, iteration+1);
             }
         }
         // recur back if there's no safe place for the queen
-        if(column !=0) depthLimitedSearch(board, limit, column-1, iteration+1);
+        board[(int)(Math.random()*(7 +1)+0)  ][column] = 1;
+        State s;
+        if(column !=0) s= depthLimitedSearch(board, limit, column-1, iteration+1);
+        else s=depthLimitedSearch(board, limit, findSafe(board, column), iteration+1);
+        if(s.equals(State.TRUE)) return State.TRUE;
+        if(s.equals(State.CUTOFF)) return State.CUTOFF;
         return State.FALSE;
     }
-
     /* This function solves the N Queen problem using
        Backtracking.  It mainly uses solveNQUtil () to
        solve the problem. It returns false if queens
@@ -101,9 +133,9 @@ public class IDSearch {
        Please note that there may be more than one
        solutions, this function prints one of the
        feasible solutions.*/
-    public boolean solveNQ(int[][] board)
+    public boolean solveNQ(int[][] board, int maximumDepth, int step)
     {
-        if (!iterativeDepthSearch(board, 16, 2)) {
+        if (!iterativeDepthSearch(board, maximumDepth, step)) {
             System.out.print("Solution does not exist");
             return false;
         }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,19 +2,19 @@ import IDS.IDSearch;
 
 public class Main {
 
-    static int board[][] ={{ 1, 0, 0, 0, 0, 0 ,0, 0 },
-            { 0, 1, 0, 0, 0, 0 ,1, 0 },
-            { 0, 0, 0, 0, 0, 0 ,0, 1 },
-            { 0, 0, 0, 1, 0, 0 ,0, 0 },
-            { 0, 0, 0, 0, 1, 0 ,0, 0 },
-            { 0, 0, 1, 0, 0, 0 ,0, 0 },
-            { 0, 0, 0, 0, 0, 1 ,0, 0 },
-            { 0, 0, 0, 0, 0, 0 ,0, 0 },};
+    static int board[][] ={ { 0, 0, 0, 0, 0, 0 ,0, 0 },
+                            { 0, 0, 0, 0, 0, 0 ,1, 0 },
+                            { 0, 0, 0, 0, 0, 0 ,0, 1 },
+                            { 0, 0, 0, 0, 0, 0 ,0, 0 },
+                            { 0, 1, 0, 0, 0, 0 ,0, 0 },
+                            { 0, 0, 0, 1, 1, 0 ,0, 0 },
+                            { 0, 0, 0, 0, 0, 1 ,0, 0 },
+                            { 1, 0, 1, 0, 0, 0 ,0, 0 },};
 
     // driver program to test above function
     public static void main(String args[])
     {
         IDSearch Queen = new IDSearch();
-        Queen.solveNQ(board);
+        Queen.solveNQ(board, 190, 2);
     }
 }
